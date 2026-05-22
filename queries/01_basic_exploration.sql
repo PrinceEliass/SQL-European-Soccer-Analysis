48578d42a66b087830ba2fbc962f2aa051f62ac0
PrinceEliass <eliasborchert1@gmail.com>
2026-05-22 19:30:33 +0300

    01_basic_exploration.sql

--- queries/01_basic_exploration.sql
+++ queries/01_basic_exploration.sql
@@ -1,1 +1,11 @@
+- 01. Basic Exploration - European Soccer Database
 
+- Description: Basic overview of the dataset (players, teams, leagues)
+- Goal: Show data volume and basic statistics
+
+SELECT 
+    (SELECT COUNT(*) FROM player) AS total_players,
+    (SELECT COUNT(*) FROM team) AS total_teams,
+    (SELECT COUNT(*) FROM league) AS total_leagues,
+    (SELECT COUNT(*) FROM player_attributes) AS total_player_attributes,
+    (SELECT COUNT(DISTINCT season) FROM match) AS total_seasons;  -- Note: if match table is empty, this may return 0
