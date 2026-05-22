2c7353d1e6edf4bd722636c7da72722fdac903a8
PrinceEliass <eliasborchert1@gmail.com>
2026-05-22 19:31:00 +0300

    02_player_performance.sql

--- queries/02_player_performance.sql
+++ queries/02_player_performance.sql
@@ -1,1 +1,11 @@
+- 02. Player Performance Analysis
 
+- Description: Overall player statistics and skill averages
+
+SELECT 
+    ROUND(AVG(overall_rating), 2) AS avg_overall_rating,
+    ROUND(AVG(potential), 2) AS avg_potential,
+    ROUND(AVG(stamina), 2) AS avg_stamina,
+    ROUND(AVG(finishing), 2) AS avg_finishing,
+    COUNT(*) AS total_records
+FROM player_attributes;
