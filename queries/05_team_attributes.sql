82f61aa02402f5314cdd10072079ad0b6ec70164
PrinceEliass <eliasborchert1@gmail.com>
2026-05-22 19:32:09 +0300

    05_team_attributes.sql

--- queries/05_team_attributes.sql
+++ queries/05_team_attributes.sql
@@ -1,1 +1,15 @@
+- 05. Team Attributes Analysis
 
+- Description: Comparison of team playing styles
+
+SELECT 
+    t.team_long_name,
+    ROUND(AVG(ta.buildUpPlaySpeed), 2) AS avg_buildup_speed,
+    ROUND(AVG(ta.chanceCreationPassing), 2) AS avg_chance_creation,
+    ROUND(AVG(ta.defencePressure), 2) AS avg_defence_pressure,
+    COUNT(*) AS records
+FROM team_attributes ta
+JOIN team t ON t.team_api_id = ta.team_api_id
+GROUP BY t.team_long_name
+ORDER BY avg_chance_creation DESC
+LIMIT 10;
